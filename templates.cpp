401e3557736055240a7329879f40d5bdddb9f7f2
natali1821 <ermolina_natali@bk.ru>
2021-06-06 22:54:13 +0300

    Add templates.cpp

---
+++ templates.cpp
@@ -0,0 +1,17 @@
+#include "MyVector.h"
+#include "MyVector.cpp"
+#include "VectorIterator.cpp"
+#include "ConstVectorIterator.cpp"
+
+template class MyVector<char>;
+template class MyVector<unsigned char>;
+template class MyVector<short>;
+template class MyVector<int>;
+template class MyVector<long>;
+template class MyVector<long long>;
+template class MyVector<unsigned short>;
+template class MyVector<unsigned int>;
+template class MyVector<unsigned long>;
+template class MyVector<unsigned long long>;
+template class MyVector<float>;
+template class MyVector<double>;
